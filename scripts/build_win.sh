e6b1d37cd25f35a4c926f7dc3505ca392b41859f
neverwasd <149199299+neverwasd@users.noreply.github.com>
2024-11-01 20:33:04 -0600

    Refactor for PCbuild.bat

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -1,16 +1,29 @@
 set -euo pipefail
 
-# Python dependencies
-pacman -S --needed --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-zlib mingw-w64-x86_64-openssl mingw-w64-x86_64-libffi mingw-w64-x86_64-bzip2 mingw-w64-x86_64-winpthreads
+# Configure MSVC path
+MSVC_PATH="/c/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64"
+export PATH="${MSVC_PATH}:$PATH"
+
+# Python
+PYTHON_VERSION=3.11.9
+PYTHON_NAME=Python-${PYTHON_VERSION}
+PYTHON_ARCHIVE=${PYTHON_NAME}.tgz
+wget --progress=dot:giga https://www.python.org/ftp/python/${PYTHON_VERSION}/${PYTHON_ARCHIVE}
+echo "e7de3240a8bc2b1e1ba5c81bf943f06861ff494b69fda990ce2722a504c6153d  ./${PYTHON_ARCHIVE}" | sha256sum -c -
+tar -xf ${PYTHON_ARCHIVE}
+PYTHON_SRC_DIR=${PWD}/${PYTHON_NAME}
+cd "${PYTHON_SRC_DIR}"
+./PCbuild/build.bat -e -d -p x64
+cd ..
 
 # llvm
 LLVM_NAME=clang+llvm-18.1.5-x86_64-pc-windows-msvc
-LLVM_ARCHIVE="$LLVM_NAME.tar.xz"
-wget --progress=dot:giga https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.5/$LLVM_ARCHIVE
-echo "7027f03bcab87d8a72fee35a82163b0730a9c92f5160373597de95010f722935  ./$LLVM_ARCHIVE" | sha256sum -c -
-tar -xf $LLVM_ARCHIVE
-export LLVM_INSTALL_DIR=$PWD/$LLVM_NAME
-export CMAKE_PREFIX_PATH=$LLVM_INSTALL_DIR
+LLVM_ARCHIVE="${LLVM_NAME}.tar.xz"
+wget --progress=dot:giga https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.5/${LLVM_ARCHIVE}
+echo "7027f03bcab87d8a72fee35a82163b0730a9c92f5160373597de95010f722935  ./${LLVM_ARCHIVE}" | sha256sum -c -
+tar -xf ${LLVM_ARCHIVE}
+export LLVM_INSTALL_DIR=${PWD}/{$LLVM_NAME}
+export CMAKE_PREFIX_PATH=${LLVM_INSTALL_DIR}
 
 # REMOVE any gcc installs (possibly provided by msys) from path, we are trying to do a MSVC based build
 which cl
@@ -19,4 +32,4 @@ export PATH=`echo $PATH | tr ":" "\n" | grep -v "mingw64" | grep -v "Strawberry"
 echo $PATH
 which gcc || echo "No GCC in path, OK!"
 
-make PLATFORM=win
+make PLATFORM=win "PYTHON_WINDOWS=${PYTHON_SRC_DIR}"
